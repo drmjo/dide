15e6d2be0533294168df765e038cb7763052a7b9
Majan Paul <drmjo007@gmail.com>
2020-01-20 14:49:16 -0800

    make sure only the symvers get truncated

--- build.sh
+++ build.sh
@@ -8,13 +8,25 @@ REGISTRY=docker.io/drmjo
 
 IMG=${REGISTRY}/${APP}
 FQIN=${IMG}:${VERSION}
+TAGS="-t ${FQIN}"
 
-VERSION_SHORT=$(echo $VERSION | awk -F'.' '{print $1"."$2}')
-FQIN_SHORT=${IMG}:${VERSION_SHORT}
+VERSION_REGEX="([0-9]*.[0-9]*).([0-9]*)"
+
+# check if the version can be shortened
+# i.e. 1.16.7 will be converted into 1.16
+if [[ $VERSION =~ $VERSION_REGEX ]]; then
+    echo "Version needs to be shortened"
+    VERSION_SHORT=${BASH_REMATCH[1]}
+    FQIN_SHORT=${IMG}:${VERSION_SHORT}
+    TAGS+=" -t ${FQIN_SHORT}"
+fi
+
+echo $FQIN_SHORT
+echo $VERSION_SHORT
+echo $TAGS
 
 docker build \
-    -t ${FQIN} \
-    -t ${FQIN_SHORT} \
+    ${TAGS} \
     --build-arg REGISTRY=${REGISTRY} \
     --build-arg SHA256SUM=${SHA256SUM} \
     --build-arg AWS_CLI_VERSION=${AWS_CLI_VERSION} \
@@ -22,4 +34,6 @@ docker build \
     ${APP}
 
 docker push ${FQIN}
-docker push ${FQIN_SHORT}
+
+# push short version too if FQIN_SHORT is set
+[[ -n ${FQIN_SHORT} ]] && docker push ${FQIN_SHORT}
